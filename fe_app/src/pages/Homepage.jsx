6c779a3a5c6b5399213c3abcabee8480b80692ea
GiordanoCasale <giordanocasale8@gmail.com>
2025-06-12 11:33:40 +0200

    modifica fe-1

--- fe_app/src/pages/Homepage.jsx
+++ fe_app/src/pages/Homepage.jsx
@@ -2,45 +2,43 @@ import React from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 import { useState, useEffect, useContext } from 'react'
-import MovieCard from '../components/MovieCard'
+import WineCard from '../components/WineCard'
 import GlobalContext from '../contexts/globalContext'
 
-
-
 const Homepage = () => {
-    const [movies, setMovies] = useState([]);
-    const { setIsLoading } = useContext(GlobalContext)
+    const [wines, setWines] = useState([]);
+    const { setIsLoading } = useContext(GlobalContext);
 
-
-    const fetchMovies = () => {
-        setIsLoading(true)
-        axios.get('http://localhost:3000/movies')
+    const fetchWines = () => {
+        setIsLoading(true);
+        axios.get('http://localhost:3000/wines', { timeout: 20000 })
             .then((response) => {
                 console.log(response.data);
-                setMovies(response.data);
+                setWines(response.data);
                 setIsLoading(false);
-            }, 20000)
-            .catch((error) => {
-                console.log(error)
             })
-    }
+            .catch((error) => {
+                console.log(error);
+                setIsLoading(false);
+            });
+    };
 
     useEffect(() => {
-        fetchMovies()
-    }, [])
+        fetchWines();
+    }, []);
 
     return (
         <>
-            <h1 className="text-primary mb-3">Bool Movies</h1>
-            <h2 className="mb-4 fst-italic">The Best Homevideo only for you</h2>
+            <h1 className="text-primary mb-3">Bool Wines</h1>
+            <h2 className="mb-4 fst-italic">The Best Homewine only for you</h2>
             <div className="row gy-4">
-                {movies.map((movie) => (
-                    <MovieCard movie={movie} key={`movie-${movie.id}`} />
+                {wines.map((wine) => (
+                    <WineCard wine={wine} key={`wine-${wine.id}`} />
                 ))}
             </div>
         </>
+    );
+};
 
-    )
-}
+export default Homepage;
 
-export default Homepage
--- fe_app/src/pages/Winepage.jsx
+++ fe_app/src/pages/Winepage.jsx
@@ -6,40 +6,41 @@ import Stars from '../components/Stars';
 import ReviewForm from '../components/ReviewForm';
 import GlobalContext from '../contexts/globalContext';
 
-const Moviepage = () => {
+const Winepage = () => {
     const { id } = useParams();
-    const [movie, setMovie] = useState({});
-    const { setIsLoading } = useContext(GlobalContext)
+    const [wine, setWine] = useState({});
+    const { setIsLoading } = useContext(GlobalContext);
 
-    const fetchMovie = () => {
-        setIsLoading(true)
-        axios.get(`http://127.0.0.1:3000/movies/${id}`)
+    const fetchWine = () => {
+        setIsLoading(true);
+        axios.get(`http://127.0.0.1:3000/wines/${id}`, { timeout: 2000 })
             .then((response) => {
                 console.log(response.data);
-                setMovie(response.data);
-                setIsLoading(false)
-            }, 2000)
+                setWine(response.data);
+                setIsLoading(false);
+            })
             .catch((error) => {
                 console.log(error);
+                setIsLoading(false);
             });
     };
 
     useEffect(() => {
-        fetchMovie();
+        fetchWine();
     }, []);
 
     return (
         <div className="container py-4">
             <div className="row mb-4">
                 <div className="col-12 col-md-6 col-lg-4 mb-3">
-                    <img src={movie.image} className="img-fluid rounded shadow" alt={movie.title} />
+                    <img src={wine.image} className="img-fluid rounded shadow" alt={wine.title} />
                 </div>
                 <div className="col-12 col-md-6 col-lg-8">
-                    <h1 className="mb-2">{movie.title}</h1>
-                    <h5 className="text-muted">Regia: {movie.director}</h5>
-                    <Stars vote={movie.average_vote || 0} />
-                    <h6 className="mt-3">Anno: {movie.release_year}</h6>
-                    <p className="mt-3">{movie.description}</p>
+                    <h1 className="mb-2">{wine.title}</h1>
+                    <h5 className="text-muted">Cantina: {wine.director}</h5>
+                    <Stars vote={wine.average_vote || 0} />
+                    <h6 className="mt-3">Anno: {wine.release_year}</h6>
+                    <p className="mt-3">{wine.description}</p>
                 </div>
             </div>
 
@@ -48,8 +49,8 @@ const Moviepage = () => {
                     <h3 className="mb-3">Recensioni della community</h3>
                 </div>
 
-                {movie.reviews && movie.reviews.length > 0 ? (
-                    movie.reviews.map((review) => (
+                {wine.reviews && wine.reviews.length > 0 ? (
+                    wine.reviews.map((review) => (
                         <div className="col-12" key={`review-${review.id}`}>
                             <ReviewCard review={review} />
                         </div>
@@ -61,11 +62,11 @@ const Moviepage = () => {
                 )}
 
                 <div className="col-12">
-                    <ReviewForm movie_id={movie.id} reloadReviews={fetchMovie} />
+                    <ReviewForm wine_id={wine.id} reloadReviews={fetchWine} />
                 </div>
             </div>
         </div>
     );
 };
 
-export default Moviepage;
+export default Winepage;
