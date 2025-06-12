6bc0bb466cdcce5eb11e1e2236e62e2c9f0c6805
mannuccimattia <mannuccimattia@gmail.com>
2025-06-12 12:13:46 +0200

    [UPD] be_app edit

--- be_app/middlewares/imagePath.js
+++ be_app/middlewares/imagePath.js
@@ -1,5 +1,5 @@
 const imagePath = (req, res, next) => {
-  req.imagePath = `${req.protocol}://${req.get("host")}/imgs/`;
+  req.imagePath = `${req.protocol}://${req.get("host")}/imgs/`; // controllare url
   next();
 }
 
---
+++ be_app/routers/winesRouter.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+// import controller
+const winesController = require("../controllers/winesController");
+
+
+// routes
+router.get("/", winesController.index);
+
+router.get("/:id", winesController.show);
+
+// router.post("/:id/review", winesController.storeReview);
+
+module.exports = router;
