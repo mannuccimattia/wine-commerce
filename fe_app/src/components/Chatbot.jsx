0ef32c04a805b10a4b7a9198ba48b43b0ed83389
tizianotambone <tambonetiziano@gmail.com>
2025-06-14 11:39:52 +0200

    [UPD] defaultlayaut chatbot

---
+++ fe_app/src/components/Chatbot.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Chatbot = () => {
+  
+  return (
+    <>  
+    
+    <div id='chatbot'>
+      <i class="fa-regular fa-comment-dots"></i>
+    </div>
+
+    
+    </>
+
+    
+  )
+}
+
+export default Chatbot
