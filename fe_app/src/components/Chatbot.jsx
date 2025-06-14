57de769dd03598396ec308297f513ef3f9ff6f00
tizianotambone <tambonetiziano@gmail.com>
2025-06-14 12:28:46 +0200

    [UPD]layout toggle

--- fe_app/src/components/Chatbot.jsx
+++ fe_app/src/components/Chatbot.jsx
@@ -1,18 +1,38 @@
 import React from 'react'
+import { useState } from 'react'
+
 
 const Chatbot = () => {
-  
-  return (
-    <>  
-    
-    <div id='chatbot'>
-      <i class="fa-regular fa-comment-dots"></i>
-    </div>
+  const [isActive, setIsActive] = useState(false);
 
-    
+  return (
+    <>
+      {/* icon */}
+      <div id='chatbot' onClick={() => setIsActive(!isActive)}>
+        <i class="fa-regular fa-comment-dots"></i>
+      </div>
+
+      {/* chat */}
+      {
+        (isActive) 
+        ? ( <div id='chatbot-modal' className='p-2'>
+          <div className='chatbot-header d-flex justify-content-between p-2 align-items-center'>
+            <h2> Parla con noi</h2>
+            <i class="fa-solid fa-xmark cursor-pointer" onClick={() => setIsActive(!isActive)}></i>
+          </div>
+          <div className='chatbot-body'>
+            <p>in che modo posso esserti utile?</p>
+          </div>
+          <div className='chatbot-footer d-flex justify-content-between align-items-center py-2 gap-1'>
+            <input type="text" placeholder='Type your message here...' className='input-msg'/>
+            <button className='btn-msg'><i class="fa-regular fa-paper-plane"></i></button>
+          </div>
+        </div>
+      ) : (null)
+    }
     </>
 
-    
+
   )
 }
 
