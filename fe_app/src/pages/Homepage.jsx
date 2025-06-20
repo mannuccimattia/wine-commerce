eeda977aafd91818197f1085de1b5dc1177aaf31
GiordanoCasale <giordanocasale8@gmail.com>
2025-06-20 15:01:08 +0200

    [HOTFIX] rimossi degli import in homepage che non permettono la visione del sito

--- fe_app/src/pages/Homepage.jsx
+++ fe_app/src/pages/Homepage.jsx
@@ -1,7 +1,4 @@
 // import axios from 'axios'
-import { useState, useContext } from 'react'
-import { useNavigate } from 'react-router-dom';
-import GlobalContext from '../contexts/globalContext'
 import CategoriesCard from '../components/CategoriesCard'
 import HeroCarousel from '../components/HeroCarousel';
 import BestSellers from '../components/BestSellers';
